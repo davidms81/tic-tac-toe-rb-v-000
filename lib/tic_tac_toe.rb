d3e9e5bd896a12a88d7dcc2794afa8ccbfae54d2
David Schoenholtz <dmschoenholtz@gmail.com>
2020-04-21 19:34:39 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -21,6 +21,6 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def player_move(board, position, token)
-  board[position] = token
+def player_move(board, position, player)
+  board[position] = player
 end
